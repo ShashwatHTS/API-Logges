6ae3f177affb85c412b0be80986b7d257da91e46
shashwatHTSgit config --global user.email shashwat.pal@htstechsolutions.com <shashwat.pal@htstechsolutions.com>
2024-01-02 15:38:06 +0530

    h

--- src/index.js
+++ src/index.js
@@ -4,8 +4,32 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express()
 
+const logger = require('./logger.js');
+
+
 app.set('view engine', 'ejs')
 
+app.use((req, res, next) => {
+  const startTime = new Date();
+
+  // Log the incoming request
+  logger.info(`${req.method} ${req.url}`);
+
+  // Override the res.json method to log the response data
+  const originalJson = res.json;
+  res.json = function (data) {
+    const endTime = new Date();
+    const elapsedTime = endTime - startTime;
+
+    // Log information about the API call
+    logger.info(`API: ${req.url}, Status: ${res.statusCode}, Elapsed Time: ${elapsedTime}ms, Response: ${JSON.stringify(data)}`);
+
+    // Call the original res.json method
+    originalJson.call(res, data);
+  };
+
+  next();
+});
 
 app.use(
   cors({
@@ -19,9 +43,6 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 4000
 
-app.get("/", async(req,res)=>{
-  res.send('hello')
-})
 
 
 app.use("/api/v1/books", require("./routes/book.routes.js"))

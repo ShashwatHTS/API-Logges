727e1a80d5d3a0ee90718990e627254538d3e594
shashwatHTSgit config --global user.email shashwat.pal@htstechsolutions.com <shashwat.pal@htstechsolutions.com>
2024-01-02 14:15:20 +0530

    code push

---
+++ src/controller/books.controllers.js
@@ -0,0 +1,139 @@
+require('dotenv').config()
+const books = require("../db/books.db")
+const { createClient } = require('@supabase/supabase-js');
+const { supabaseUrl, supabaseKey } = require('../db/books.db')
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+exports.getData = async (req, res) => {
+  const { data, error } = await supabase
+    .from('todo')
+    .select('*');
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // console.log(data);
+  res.send(data)
+}
+
+exports.createBook = async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from('todo')
+      .insert(
+        req.body,
+      )
+      .select()
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // console.log(data);
+    res.send(data)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error posting data to Supabase');
+  }
+}
+
+exports.getBookById = async (req, res, next) => {
+  // find a book by id
+  let { data, error } = await supabase.from('todo').select("*")
+    .eq('id', req.params.id)
+  console.log("show", data)
+  if (!data) {
+    return res.status(404).send("Book not found")
+  }
+  if (error) {
+    console.log(error)
+  } else {
+    res.send(data)
+  }
+}
+
+exports.updateBook = async (req, res, next) => {
+  // update a book
+  try {
+    let { data, error } = await supabase.from('todo').select("*")
+      .eq('id', req.params.id)
+    console.log("show", data)
+    if (!data) {
+      return res.status(404).send("Book not found")
+    }
+    if (error) {
+      console.log(error)
+    } else {
+      data = await supabase
+        .from('todo')
+        .update(req.body)
+        .eq('id', req.params.id)
+        .select()
+      res.send(data.data)
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+exports.deleteBook = async (req, res, next) => {
+  // delete a book
+  try {
+    let { data, error } = await supabase.from('todo').select("*")
+      .eq('id', req.params.id)
+    console.log("show", data)
+    if (!data) {
+      return res.status(404).send("Book not found")
+    }
+    if (error) {
+      console.log(error)
+    } else {
+      await supabase
+        .from('todo')
+        .delete()
+        .eq('id', req.params.id)
+      res.send("delete successfully")
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+exports.addToCart = async (req, res, next) => {
+  try {
+    let { data, error } = await supabase.from('todo').select("*")
+      .eq('id', req.params.id)
+    console.log("show", data)
+    if (!data) {
+      return res.status(404).send("Book not found")
+    }
+    if (error) {
+      console.log(error)
+    }
+    data = await supabase
+      .from('cart')
+      .insert(
+        req.body,
+      )
+      .eq('id', req.params.id)
+      .select()
+    console.log(data.data);
+    res.send(data)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+exports.getCartData = async (req, res) => {
+  const { data, error } = await supabase
+    .from('cart')
+    .select('*');
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // console.log(data);
+  res.send(data)
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const port = process.env.PORT || 4000
+
+
+app.use("/api/v1/books", require("./routes/book.routes.js"))
+
+app.use("/api/v1/auth", require("./routes/auth.routes.js"))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+module.exports = app
